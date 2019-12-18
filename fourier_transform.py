21b0262eee9aa54e23b3004f2528b2affc2d7613
Ben Dixon <ben@Bens-MBP.home>
2019-12-18 17:01:00 +0000

    Add functionality for splitting video into collection of frames for analysis and to convert collection of frames into collection of fourier transforms

--- fourier_transform.py
+++ fourier_transform.py
@@ -3,45 +3,49 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-# Facial detection/extraction
-img = cv2.imread('images/real/real_full_frame_1.png', 0)
+# Split video into frames
+vidcap = cv2.VideoCapture('599_585.mp4')
+count = 0
+success = True
+while success:
+  success,image = vidcap.read()
+  cv2.imwrite("frames/frame%d.jpg" % count, image)
+  print ('Read a new frame: ', success)
+  count += 1
 
+# Facial detection/extraction
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-faces = faceCascade.detectMultiScale(
-    img,
-    scaleFactor=2
-)
+for frameNumber in range(0, count):
+    img = cv2.imread('frames/frame{}.jpg'.format(frameNumber), 0)
 
-print("Found {0} faces!".format(len(faces)))
+    faces = faceCascade.detectMultiScale(
+        img,
+        scaleFactor=1.85
+    )
 
-# Crop background so that the image is just the face
-i = 0
-for (x, y, w, h) in faces:
-    r = max(w, h) / 2
-    centerx = x + w / 2
-    centery = y + h / 2
-    nx = int(centerx - r)
-    ny = int(centery - r)
-    nr = int(r * 2)
+    print("Found {0} faces!".format(len(faces)))
 
-    faceimg = img[ny:ny+nr, nx:nx+nr]
-    #lastimg = cv2.resize(faceimg, (32, 32))
-    i += 1
-    cv2.imwrite("image%d.png" % i, faceimg)
+    # Crop background so that the image is just the face
 
-#cv2.imshow("Faces found", img)
-#cv2.waitKey(0)
+    i = 0
+    for (x, y, w, h) in faces:
+        r = max(w, h) / 2
+        centerx = x + w / 2
+        centery = y + h / 2
+        nx = int(centerx - r)
+        ny = int(centery - r)
+        nr = int(r * 2)
 
-#FFT algorithm
+        faceimg = img[ny:ny+nr, nx:nx+nr]
+        #smallimg = cv2.resize(faceimg, (32, 32))
+        i += 1
+        cv2.imwrite("frames_faces/frame{}_{}.jpg".format(frameNumber, i), faceimg)
 
-f = np.fft.fft2(faceimg)
-fshift = np.fft.fftshift(f)
-magnitude_spectrum = np.log(np.abs(fshift))
+        #FFT algorithm
+        f = np.fft.fft2(faceimg)
+        fshift = np.fft.fftshift(f)
+        magnitude_spectrum = 5*np.log(np.abs(fshift))
+        cv2.imwrite("frames_fft/frame{}_{}.jpg".format(frameNumber, i), magnitude_spectrum)
 
-plt.figure()
-plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-plt.show()
+    frameNumber = frameNumber + 1
