85e2b0c0dc5de532dd8f254b9b97aefb74112e0e
Ben Dixon <ben.dixon300@gmail.com>
2020-01-26 11:24:57 +0000

    Using mtcnn_pytorch facial detection instead of opencv2, and attempt to remove noisy faces

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import numpy as np
 import pandas as pd 
+from PIL import Image
+import better_facial_detection as fd
 from matplotlib import pyplot as plt
 
 def generateFFT(path, label, video_number):
@@ -9,12 +11,17 @@ def generateFFT(path, label, video_number):
     df = pd.DataFrame({'frame': [], 'label': []})
     video_count = 0
     for filename in os.listdir(path):
+        # Uncomment to convert a specific video file
+        """if filename != "09__walking_down_street_outside_angry.mp4":
+            print("skipping {}".format(filename))
+            continue"""
         if video_count > video_number - 1:
             break
-        elif video_count < 5:
+        # Uncomment to skip video files
+        """elif video_count < 6:
             print("skipping {} as its in the testing set".format(filename))
             video_count = video_count + 1
-            continue
+            continue"""
         frame_count = 0
         print("Generating FFT for video: {}".format(filename))
         # Split fake video into frames
@@ -22,60 +29,58 @@ def generateFFT(path, label, video_number):
         success = True
         while success:
             success,image = vidcap.read()
-            cv2.imwrite("frames/frame{}_{}_{}.jpg".format(video_count, frame_count, label), image)
-            #print ('Read a new frame: ', success)
+            if not success:
+                break
+            cropped_faces = fd.MTCNNFacialDetection(Image.fromarray(image), video_count, frame_count, label)
             frame_count += 1
-
-        # Facial detection/extraction
-        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-        for frameNumber in range(0, frame_count):
-            img = cv2.imread('frames/frame{}_{}_{}.jpg'.format(video_count, frameNumber, label), 0)
-
-            faces = faceCascade.detectMultiScale(
-                img,
-                scaleFactor=1.85
-            )
-
-            #print("Found {0} faces!".format(len(faces)))
 
             # Crop background so that the image is just the face
             j = 0
-            for (x, y, w, h) in faces:
-                r = max(w, h) / 2
-                centerx = x + w / 2
-                centery = y + h / 2
-                nx = int(centerx - r)
-                ny = int(centery - r)
-                nr = int(r * 2)
-
-                faceimg = img[ny:ny+nr, nx:nx+nr]
-                faceimg = cv2.resize(faceimg, (281, 281))
+            for face in cropped_faces:
+                face = np.array(face)
+                face = cv2.resize(face, (281, 281))
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
                 j += 1
-                cv2.imwrite("frames_faces/frame{}_{}_{}_{}.jpg".format(video_count, frameNumber, j, label), faceimg)
+                #cv2.imwrite("frames_faces/frame{}_{}_{}_{}.jpg".format(video_count, frame_count, j, label), face)
 
                 #FFT algorithm
-                f = np.fft.fft2(faceimg)
+                f = np.fft.fft2(face)
                 fshift = np.fft.fftshift(f)
                 # The magnitude size is likely to be a hyper parameter in this case
                 magnitude_spectrum = 10*np.log(np.abs(fshift))
-                frame_file_name = 'frame{}_{}_{}_{}.jpg'.format(video_count, frameNumber, j, label)
+                frame_file_name = 'frame{}_{}_{}_{}.jpg'.format(video_count, frame_count, j, label)
                 df = df.append({'frame': frame_file_name, 'label': label}, ignore_index=True)
                 df["label"] = df["label"].astype(int)
                 # Create FFT representation of face
                 cv2.imwrite('frames_fft/' + frame_file_name, magnitude_spectrum)
+                # Only get the first face in the image
+                break
 
         video_count = video_count + 1
-        if os.path.isfile('example.csv'):
+        if os.path.isfile('labels.csv'):
             print("appending to csv")
-            df.to_csv('example.csv', header=None, mode='a')
+            df.to_csv('labels.csv', header=None, mode='a')
         else:
             print("Creating new csv")
-            df.to_csv('example.csv')
+            df.to_csv('labels.csv')
 
-generateFFT('/Volumes/antideepfake/FaceForensics++dataset_larger_compressed/manipulated_sequences/Deepfakes/c23/videos/', 0, 20)
-generateFFT('/Volumes/antideepfake/FaceForensics++dataset_larger_compressed/original_sequences/youtube/c23/videos/', 1, 20)
 
-#generateFFT('/Volumes/antideepfake/FaceForensics++dataset/manipulated_sequences/Deepfakes/raw/videos/', 0, 5)
-#generateFFT('/Volumes/antideepfake/FaceForensics++dataset/original_sequences/youtube/raw/videos/', 1, 5)
+def clear_fft_directory():
+    for filename in os.listdir('frames_faces'):
+        os.remove('frames_faces/' + filename)
+    for filename in os.listdir('frames_fft'):
+        os.remove('frames_fft/' + filename)
+
+
+clear_fft_directory()
+print("cleared")
+# Generate FFT images from youtube data
+#generateFFT('/Volumes/antideepfake/FaceForensics++dataset_larger_compressed/manipulated_sequences/Deepfakes/c23/videos/', 0, 5)
+#generateFFT('/Volumes/antideepfake/FaceForensics++dataset_larger_compressed/original_sequences/youtube/c23/videos/', 1, 5)
+
+# Generate FFT images from actor data
+generateFFT('/Volumes/antideepfake/FaceForensics++dataset_larger_compressed/manipulated_sequences/DeepFakeDetection/c23/videos/', 0, 5)
+generateFFT('/Volumes/antideepfake/FaceForensics++dataset_larger_compressed/original_sequences/actors/c23/videos/', 1, 5)
+
+
 
